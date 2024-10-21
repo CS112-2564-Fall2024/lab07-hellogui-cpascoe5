adca90e383bfe465f9c7dc8479dbb31826f7625b
Connor P <connorp305@gmail.com>
2024-10-21 14:34:59 -0700

    Inital Commmit

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -26,6 +26,7 @@ public class HelloApplication extends Application  { //inheriting core functiona
         Scene scene = new Scene(layout, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
